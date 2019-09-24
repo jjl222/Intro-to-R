ace358107177f78a7d2e301de1c8cb1fa59b7fd6
Josh Luchini <jjl222@lehigh.edu>
2019-09-24 14:41:11 -0400

    Edited Intro to R.R

--- Intro to R.R
+++ Intro to R.R
@@ -1,5 +1,5 @@
 # R script to accompany Intro to R
-# Josh Luchini
+# Josh Luchin
 ######################################################
 
 # You will see that you can put non-executable "comment" lines
